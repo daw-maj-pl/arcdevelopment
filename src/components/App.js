d44a7b3c4a9481e4e483b2d6f56fb977f37887ea
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-08-25 11:23:27 +0200

    Add new directories - assets, animations components

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import { ThemeProvider } from '@material-ui/core/styles';
+
+import theme from './ui/Theme';
+import Header from '../components/ui/Header';
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      Hello!!
+    </ThemeProvider>
+  );
+}
+
+export default App;
