b21c37792be93886bc528d4b42e50de1b917d53c
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-09-02 14:36:26 +0200

    Customize the theme: h4, subtitle1, learnButton

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -2,6 +2,7 @@ import { createTheme } from '@material-ui/core/styles';
 
 const arcBlue = '#0B72B9';
 const arcOrange = '#FFBA60';
+const arcGrey = '#868686';
 
 export default createTheme({
   palette: {
@@ -33,8 +34,28 @@ export default createTheme({
       fontFamily: 'Raleway',
       fontWeight: 700,
       fontSize: '2.5rem',
-      color: `${arcBlue}`,
+      color: arcBlue,
       lineHeight: '1.5'
+    },
+    h4: {
+      fontFamily: 'Raleway',
+      fontSize: '1.75rem',
+      color: arcBlue,
+      fontWeight: 700
+    },
+    subtitle1: {
+      fontSize: '1.25rem',
+      fontWeight: 300,
+      color: arcGrey
+    },
+    learnButton: {
+      borderColor: arcBlue,
+      color: arcBlue,
+      borderWidth: 2,
+      textTransform: 'none',
+      borderRadius: 50,
+      fontFamily: 'Roboto',
+      fontWeight: 'bold'
     }
   }
 });
