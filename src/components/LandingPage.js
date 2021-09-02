d70e58be4ece002c522926d5322be4f958605cee
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-09-02 14:30:09 +0200

    Create Services Block - Custom Software section

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,8 +4,10 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import ButtonArrow from './ui/ButtonArrow';
 import Typography from '@material-ui/core/Typography';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import animationData from '../animations/landinganimation/data';
+import customSoftwareIcon from '../assets/Custom Software Icon.svg';
 
 const useStyles = makeStyles(theme => ({
   animation: {
@@ -32,16 +34,19 @@ const useStyles = makeStyles(theme => ({
     marginTop: '1em'
   },
   learnButtonHero: {
-    borderColor: theme.palette.common.blue,
-    color: theme.palette.common.blue,
-    borderWidth: 2,
-    textTransform: 'none',
-    borderRadius: 50,
-    fontFamily: 'Roboto',
-    fontWeight: 'bold',
+    ...theme.typography.learnButton,
     fontSize: '0.9rem',
     height: 45,
     width: 145
+  },
+  learnButton: {
+    ...theme.typography.learnButton,
+    fontSize: '0.7rem',
+    height: 35,
+    padding: 5,
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: '2em'
+    }
   },
   mainContainer: {
     marginTop: '5em',
@@ -58,12 +63,32 @@ const useStyles = makeStyles(theme => ({
     [theme.breakpoints.down('xs')]: {
       marginLeft: 0
     }
+  },
+  specialText: {
+    fontFamily: 'Pacifico',
+    color: theme.palette.common.orange
+  },
+  subtitle: {
+    marginBottom: '1em'
+  },
+  icon: {
+    marginLeft: '2em',
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: 0
+    }
+  },
+  serviceContainer: {
+    marginTop: '12em',
+    [theme.breakpoints.down('sm')]: {
+      padding: 25
+    }
   }
 }));
 
 export default function LandingPage() {
   const classes = useStyles();
   const theme = useTheme();
+  const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
 
   const defaultOptions = {
     loop: true,
@@ -77,6 +102,8 @@ export default function LandingPage() {
   return (
     <Grid container direction="column" className={classes.mainContainer}>
       <Grid item>
+        {' '}
+        {/*-----Hero Block-----*/}
         <Grid
           container
           justifyContent="flex-end"
@@ -102,14 +129,60 @@ export default function LandingPage() {
               </Grid>
               <Grid item>
                 <Button variant="outlined" className={classes.learnButtonHero}>
-                  <span style={{marginRight: 10}}>Learn More</span>
-                  <ButtonArrow width={15} height={15} fill={theme.palette.common.blue} />
+                  <span style={{ marginRight: 10 }}>Learn More</span>
+                  <ButtonArrow
+                    width={15}
+                    height={15}
+                    fill={theme.palette.common.blue}
+                  />
                 </Button>
               </Grid>
             </Grid>
           </Grid>
           <Grid item sm className={classes.animation}>
             <Lottie options={defaultOptions} height={'100%'} width={'100%'} />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        {' '}
+        {/*-----Services Block-----*/}
+        <Grid
+          container
+          direction="row"
+          justifyContent={matchesSM ? 'center' : undefined}
+          className={classes.serviceContainer}
+        >
+          <Grid
+            item
+            style={{
+              marginLeft: matchesSM ? 0 : '5em',
+              textAlign: matchesSM ? 'center' : undefined
+            }}
+          >
+            <Typography variant="h4">Custom Software Development</Typography>
+            <Typography variant="subtitle1" className={classes.subtitle}>
+              Save Energy. Save Time. Save Money
+            </Typography>
+            <Typography variant="subtitle1">
+              Complete digital solutions, from investigation to{' '}
+              <span className={classes.specialText}>celebration</span>
+            </Typography>
+            <Button variant="outlined" className={classes.learnButton}>
+              <span style={{ marginRight: 10 }}>Learn More</span>
+              <ButtonArrow
+                width={10}
+                height={10}
+                fill={theme.palette.common.blue}
+              />
+            </Button>
+          </Grid>
+          <Grid item>
+            <img
+              className={classes.icon}
+              alt="custom software icon"
+              src={customSoftwareIcon}
+            />
           </Grid>
         </Grid>
       </Grid>
