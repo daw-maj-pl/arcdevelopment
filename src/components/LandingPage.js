e1f6029ea09ee1e94c6cf5f51e13e7f0aeea82f3
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-09-03 15:50:58 +0200

    Create Information Block

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -13,6 +13,7 @@ import customSoftwareIcon from '../assets/Custom Software Icon.svg';
 import mobileAppsIcon from '../assets/mobileIcon.svg';
 import websitesIcon from '../assets/websiteIcon.svg';
 import revolutionBackground from '../assets/repeatingBackground.svg';
+import infoBackground from '../assets/infoBackground.svg';
 
 const useStyles = makeStyles(theme => ({
   animation: {
@@ -106,6 +107,14 @@ const useStyles = makeStyles(theme => ({
       borderRadius: 0,
       width: '100%'
     }
+  },
+  infoBackground: {
+    backgroundImage: `url(${infoBackground})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '100%',
+    width: '100%'
   }
 }));
 
@@ -113,6 +122,7 @@ export default function LandingPage() {
   const classes = useStyles();
   const theme = useTheme();
   const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
+  const matchesXS = useMediaQuery(theme.breakpoints.down('xs'));
 
   const defaultOptions = {
     loop: true,
@@ -293,6 +303,7 @@ export default function LandingPage() {
         </Grid>
       </Grid>
       <Grid item>
+        {/*-----Revolution Block-----*/}
         <Grid
           container
           style={{ height: '100em', marginTop: '12em' }}
@@ -332,6 +343,75 @@ export default function LandingPage() {
             </CardContent>
           </Card>
           <div className={classes.revolutionBackground} />
+        </Grid>
+      </Grid>
+      <Grid item>
+        {/*-----Information Block-----*/}
+        <Grid
+          container
+          style={{ height: '80em' }}
+          direction="row"
+          alignItems="center"
+        >
+          <Grid
+            item
+            container
+            style={{
+              position: 'absolute',
+              textAlign: matchesXS ? 'center' : 'inherit'
+            }}
+            direction={matchesXS ? 'column' : 'row'}
+            spacing={matchesXS ? 10 : 0}
+          >
+            <Grid
+              item
+              sm
+              style={{ marginLeft: matchesXS ? 0 : matchesSM ? '2em' : '5em' }}
+            >
+              <Grid container direction="column">
+                <Typography variant="h2" style={{ color: 'white' }}>
+                  About Us
+                </Typography>
+                <Typography variant="subtitle2">Let's get personal</Typography>
+                <Grid item>
+                  <Button
+                    variant="outlined"
+                    style={{ color: 'white', borderColor: 'white' }}
+                    className={classes.learnButton}
+                  >
+                    <span style={{ marginRight: 10 }}>Learn More</span>
+                    <ButtonArrow width={10} height={10} fill="white" />
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid
+              item
+              sm
+              style={{
+                marginRight: matchesXS ? 0 : matchesSM ? '2em' : '5em',
+                textAlign: matchesXS ? 'center' : 'right'
+              }}
+            >
+              <Grid container direction="column">
+                <Typography variant="h2" style={{ color: 'white' }}>
+                  Contact Us
+                </Typography>
+                <Typography variant="subtitle2">Say Hello! 👋</Typography>
+                <Grid item>
+                  <Button
+                    variant="outlined"
+                    style={{ color: 'white', borderColor: 'white' }}
+                    className={classes.learnButton}
+                  >
+                    <span style={{ marginRight: 10 }}>Learn More</span>
+                    <ButtonArrow width={10} height={10} fill="white" />
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          <div className={classes.infoBackground} />
         </Grid>
       </Grid>
     </Grid>
