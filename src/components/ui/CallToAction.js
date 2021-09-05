a2a3ccc63c9dd8cb2a28886a82dcac9567c03b22
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-09-05 15:06:35 +0200

    Add event listeners and hover effect to buttons

--- src/components/ui/CallToAction.js
+++ src/components/ui/CallToAction.js
@@ -1,6 +1,7 @@
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import ButtonArrow from './ButtonArrow';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
@@ -40,6 +41,9 @@ const useStyles = makeStyles(theme => ({
     fontSize: '1.5rem',
     marginRight: '5em',
     marginLeft: '2em',
+    '&:hover': {
+      backgroundColor: theme.palette.secondary.light
+    },
     [theme.breakpoints.down('sm')]: {
       marginRight: 0,
       marginLeft: 0
@@ -47,7 +51,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export default function CallToAction() {
+export default function CallToAction(props) {
   const classes = useStyles();
   const theme = useTheme();
   const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
@@ -82,7 +86,13 @@ export default function CallToAction() {
               justifyContent={matchesSM ? 'center' : undefined}
               item
             >
-              <Button variant="outlined" className={classes.learnButton}>
+              <Button
+                component={Link}
+                to="/revolution"
+                variant="outlined"
+                className={classes.learnButton}
+                onClick={() => props.setValue(2)}
+              >
                 <span style={{ marginRight: 5 }}>Learn More</span>
                 <ButtonArrow
                   width={10}
@@ -95,7 +105,13 @@ export default function CallToAction() {
         </Grid>
       </Grid>
       <Grid item>
-        <Button variant="contained" className={classes.estimateButton}>
+        <Button
+          component={Link}
+          to="/estimate"
+          variant="contained"
+          className={classes.estimateButton}
+          onClick={() => props.setValue(5)}
+        >
           Free Estimate
         </Button>
       </Grid>
