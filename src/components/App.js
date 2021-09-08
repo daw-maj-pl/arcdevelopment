f986896b4f64e1358bd04b3e1667b3f3836537d5
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-09-08 14:37:47 +0200

    Import the Website component

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ import LandingPage from './LandingPage';
 import Services from './Services';
 import CustomSoftware from './CustomSoftware';
 import MobileApps from './MobileApps';
+import Websites from './Websites';
 
 function App() {
   const [selectedIndex, setSelectedIndex] = useState(0);
@@ -68,7 +69,17 @@ function App() {
               />
             )}
           />
-          <Route exact path="/websites" component={() => <div>Websites</div>} />
+          <Route
+            exact
+            path="/websites"
+            render={props => (
+              <Websites
+                {...props}
+                setValue={setValue}
+                setSelectedIndex={setSelectedIndex}
+              />
+            )}
+          />
           <Route
             exact
             path="/revolution"
