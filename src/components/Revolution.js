824c81f2f8e588773239b3d1e5f031befc73e57a
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-09-09 12:59:16 +0200

    Add Technology section

--- src/components/Revolution.js
+++ src/components/Revolution.js
@@ -1,8 +1,10 @@
+import Lottie from 'react-lottie';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
 import vision from '../assets/vision.svg';
+import technologyAnimation from '../animations/technologyAnimation/data.json';
 
 const useStyles = makeStyles(theme => ({
   rowContainer: {
@@ -17,6 +19,15 @@ const useStyles = makeStyles(theme => ({
 export default function Revolution() {
   const classes = useStyles();
   const theme = useTheme();
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: technologyAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
 
   return (
     <Grid container direction="column">
@@ -83,6 +94,76 @@ export default function Revolution() {
               towards that goal.
             </Typography>
           </Grid>
+        </Grid>
+      </Grid>
+      <Grid
+        item
+        container
+        direction="row"
+        alignItems="center"
+        className={classes.rowContainer}
+      >
+        <Grid
+          item
+          container
+          direction="column"
+          lg
+          style={{ maxWidth: '40em' }}
+          align="right"
+        >
+          <Grid item>
+            <Typography variant="h4" gutterBottom>
+              Technology
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="body1" paragraph>
+              In 2013, Facebook invented a new way of building websites. This
+              new system, React.js, completely revolutionizes the process and
+              practice of website development.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Instead of chaining together long individual pages, like
+              traditional websites, React websites are built with little chunks
+              of code called components. These components are faster, easier to
+              maintain, and are easily reused and customized, each serving a
+              singular purpose.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Two years later they shocked the world by releasing a similar
+              system, React Native, for producing iOS and Android apps. Instead
+              of having to master two completely separate development platforms,
+              you can leverage the knowledge you already possessed from building
+              websites and reapply it directly! This was a huge leap forward.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              This technology is now being used by companies like AirBnB,
+              Microsoft, Netflix, Pinterest, Skype, Tesla, UberEats, and when
+              Facebook purchased Instagram large portions of it were even
+              rebuilt using React.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Developers have since built on top of these systems by automating
+              project setup and deployment, allowing creators to focus as much
+              as possible on their work itself.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              These technical advancements translate into savings by
+              significantly reducing the workload and streamlining the workflow
+              for developing new pieces of software, while also lowering the
+              barrier to entry for mobile app development.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              This puts personalization in your pocket — faster, better, and
+              more affordable than ever before.
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item container justifyContent="flex-end" lg>
+          <Lottie
+            options={defaultOptions}
+            style={{ maxWidth: '40em', margin: 0 }}
+          />
         </Grid>
       </Grid>
     </Grid>
