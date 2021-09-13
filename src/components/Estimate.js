50efaf6c79b97219e67b92dc69be7be43746fcf3
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-09-13 13:03:15 +0200

    Add first question, icons and Get Estimate button

--- src/components/Estimate.js
+++ src/components/Estimate.js
@@ -32,7 +32,24 @@ import biometrics from '../assets/biometrics.svg';
 
 import estimateAnimation from '../animations/estimateAnimation/data.json';
 
-const useStyles = makeStyles(theme => {});
+const useStyles = makeStyles(theme => ({
+  icon: {
+    width: '12em',
+    height: '10em'
+  },
+  estimateButton: {
+    ...theme.typography.estimate,
+    borderRadius: 50,
+    backgroundColor: theme.palette.common.orange,
+    height: 50,
+    width: 225,
+    fontSize: '1.25rem',
+    marginTop: '5em',
+    '&:hover': {
+      backgroundColor: theme.palette.secondary.light
+    }
+  }
+}));
 
 export default function Estimate() {
   const classes = useStyles();
@@ -49,15 +66,115 @@ export default function Estimate() {
 
   return (
     <Grid container direction="row">
-      <Grid item container direction="column">
-        <Grid item>
+      <Grid item container direction="column" lg>
+        <Grid item style={{ marginTop: '2em', marginLeft: '5em' }}>
           <Typography variant="h2">Estimate</Typography>
         </Grid>
-        <Grid item>
+        <Grid
+          item
+          style={{ marginRight: '10em', maxWidth: '50em', marginTop: '7.5em' }}
+        >
           <Lottie options={defaultOptions} height="100%" width="100%" />
         </Grid>
       </Grid>
-      <Grid item container direction="column"></Grid>
+      <Grid
+        item
+        container
+        direction="column"
+        alignItems="center"
+        lg
+        style={{ marginRight: '2em', marginBottom: '25em' }}
+      >
+        <Grid item>
+          <Typography
+            variant="h2"
+            align="center"
+            style={{
+              fontWeight: 500,
+              fontSize: '2.25rem',
+              marginBottom: '2.5em',
+              marginTop: '5em'
+            }}
+            gutterBottom
+          >
+            Which service are you interested in?
+          </Typography>
+        </Grid>
+        <Grid item container>
+          <Grid item container direction="column" md>
+            <Grid item style={{ maxWidth: '12em' }}>
+              <Typography
+                variant="h6"
+                align="center"
+                style={{ marginBottom: '1em' }}
+              >
+                Custom Software Development
+              </Typography>
+            </Grid>
+            <Grid item>
+              <img
+                src={software}
+                alt="three floating screens"
+                className={classes.icon}
+              />
+            </Grid>
+          </Grid>
+          <Grid item container direction="column" md>
+            <Grid item style={{ maxWidth: '12em' }}>
+              <Typography
+                variant="h6"
+                align="center"
+                style={{ marginBottom: '1em' }}
+              >
+                iOS/Android App Development
+              </Typography>
+            </Grid>
+            <Grid item>
+              <img
+                src={mobile}
+                alt="phones and tablet outline"
+                className={classes.icon}
+              />
+            </Grid>
+          </Grid>
+          <Grid item container direction="column" md>
+            <Grid item style={{ maxWidth: '12em' }}>
+              <Typography
+                variant="h6"
+                align="center"
+                style={{ marginBottom: '1em' }}
+              >
+                Website Development
+              </Typography>
+            </Grid>
+            <Grid item>
+              <img
+                src={website}
+                alt="computer outline"
+                className={classes.icon}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          container
+          justifyContent="space-between"
+          style={{ width: '15em', marginTop: '3em' }}
+        >
+          <Grid item>
+            <img src={backArrow} alt="Previous question" />
+          </Grid>
+          <Grid item>
+            <img src={forwardArrow} alt="Next question" />
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" className={classes.estimateButton}>
+            Get Estimate
+          </Button>
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
