2aa12312e84e302e5ab230b0db398796087d6c19
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-09-15 12:26:04 +0200

    Add getTotal function to calculate estimate cost

--- src/components/Estimate.js
+++ src/components/Estimate.js
@@ -58,6 +58,12 @@ const useStyles = makeStyles(theme => ({
     border: `2px solid ${theme.palette.common.blue}`,
     marginTop: '5em',
     borderRadius: 5
+  },
+  specialText: {
+    fontFamily: 'Raleway',
+    fontWeight: 700,
+    fontSize: '1.5rem',
+    color: theme.palette.common.orange
   }
 }));
 
@@ -333,6 +339,8 @@ export default function Estimate() {
 
   const [message, setMessage] = useState('');
 
+  const [total, setTotal] = useState(0);
+
   const defaultOptions = {
     loop: true,
     autoplay: true,
@@ -446,6 +454,29 @@ export default function Estimate() {
       default:
         break;
     }
+  };
+
+  const getTotal = () => {
+    let cost = 0;
+
+    const selections = questions
+      .map(question => question.options.filter(option => option.selected))
+      .filter(question => question.length > 0);
+
+    selections.map(options => options.map(option => (cost += option.cost)));
+
+    if (questions.length > 2) {
+      const userCost = questions
+        .filter(question => question.title === 'How many users do you expect?')
+        .map(question =>
+          question.options.filter(option => option.selected)
+        )[0][0].cost;
+
+      cost -= userCost;
+      cost *= userCost;
+    }
+
+    setTotal(cost);
   };
 
   return (
@@ -574,7 +605,10 @@ export default function Estimate() {
           <Button
             variant="contained"
             className={classes.estimateButton}
-            onClick={() => setDialogOpen(true)}
+            onClick={() => {
+              setDialogOpen(true);
+              getTotal();
+            }}
           >
             Get Estimate
           </Button>
@@ -637,7 +671,8 @@ export default function Estimate() {
             </Grid>
             <Grid item>
               <Typography variant="body1" paragraph>
-                We can create this digital solution for an estimated
+                We can create this digital solution for an estimated{' '}
+                <span className={classes.specialText}>${total.toFixed(2)}</span>
               </Typography>
               <Typography variant="body1" paragraph>
                 Fill out your name, phone number, and email, place your request,
