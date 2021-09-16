ddba363f49facf2c61e99619e4692102bfc7067c
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-09-16 14:06:12 +0200

    Add selection review - custom features and users

--- src/components/Estimate.js
+++ src/components/Estimate.js
@@ -480,10 +480,12 @@ export default function Estimate() {
         .filter(question => question.title === 'How many users do you expect?')
         .map(question =>
           question.options.filter(option => option.selected)
-        )[0][0].cost;
+        )[0][0];
 
-      cost -= userCost;
-      cost *= userCost;
+      setUsers(userCost.title);
+
+      cost -= userCost.cost;
+      cost *= userCost.cost;
     }
 
     setTotal(cost);
@@ -533,6 +535,22 @@ export default function Estimate() {
         .flat();
 
       setFeatures(newFeatures);
+    }
+  };
+
+  const getCustomFeatures = () => {
+    if (questions.length > 2) {
+      const newCustomFeatures = questions
+        .filter(
+          question =>
+            question.title ===
+            'What type of custom features do you expect to need?'
+        )
+        .map(question =>
+          question.options.filter(option => option.selected)
+        )[0][0].title;
+
+      setCustomFeatures(newCustomFeatures);
     }
   };
 
@@ -667,6 +685,7 @@ export default function Estimate() {
               getTotal();
               getPlatforms();
               getFeatures();
+              getCustomFeatures();
             }}
           >
             Get Estimate
@@ -823,7 +842,9 @@ export default function Estimate() {
                     </Grid>
                     <Grid item>
                       <Typography variant="body1">
-                        Third options check
+                        The custom features will be of{' '}
+                        {customFeatures.toLowerCase()}
+                        {`, and the project will be used by about ${users} users.`}
                       </Typography>
                     </Grid>
                   </Grid>
