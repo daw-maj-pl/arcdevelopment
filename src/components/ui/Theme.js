74b4e9cfea17d2ba705a6c97b6febd063fbf38c8
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-09-18 16:26:54 +0200

    Fix typography tab color

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -22,6 +22,7 @@ export default createTheme({
       fontFamily: 'Raleway',
       textTransform: 'none',
       fontWeight: 700,
+      color: 'white',
       fontSize: '1rem'
     },
     estimate: {
