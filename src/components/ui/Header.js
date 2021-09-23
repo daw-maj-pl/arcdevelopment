8b16f2aea3ef5d5cc97e351635835d9b731820a1
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-09-23 20:33:46 +0200

    Fix a bug

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -191,28 +191,25 @@ export default function Header(props) {
     { name: 'Contact Us', link: '/contact', activeIndex: 4 }
   ];
 
+  const path = typeof window !== 'undefined' ? window.location.pathname : null;
+
+  const activeIndex = () => {
+    const found = routes.find(({ link }) => link === path);
+    const menuFound = menuOptions.find(({ link }) => link === path);
+
+    if (menuFound) {
+      props.setValue(1);
+      props.setSelectedIndex(menuFound.selectedIndex);
+    } else if (found === undefined) {
+      props.setValue(false);
+    } else {
+      props.setValue(found.activeIndex);
+    }
+  };
+
   useEffect(() => {
-    [...menuOptions, ...routes].forEach(route => {
-      switch (window.location.pathname) {
-        case `${route.link}`:
-          if (props.value !== route.activeIndex) {
-            props.setValue(route.activeIndex);
-            if (
-              route.selectedIndex &&
-              route.selectedIndex !== props.selectedIndex
-            ) {
-              props.setSelectedIndex(route.selectedIndex);
-            }
-          }
-          break;
-        case '/estimate':
-          props.setValue(5);
-          break;
-        default:
-          break;
-      }
-    });
-  }, [props.value, menuOptions, props.selectedIndex, routes, props]);
+    activeIndex();
+  }, [path]);
 
   const tabs = (
     <React.Fragment>
